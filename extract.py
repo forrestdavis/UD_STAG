7fbbf8d4183321af731279b11048867eb1a8afaf
forrestdavis <fld2111@columbia.edu>
2017-06-22 22:36:46 -0600

    Waiting for validation, but current number of trees: 8592.

--- extract.py
+++ extract.py
@@ -16,6 +16,9 @@ def Trees(sentences):
         #First pass to create hash map of sentence
         #as well as locate root child and verb locations
         for node in sentence:
+            #Skipping over enchanced nodes
+            if node[6] == "_":
+                continue
             sent_hash[int(node[0])] = (node[3], int(node[6]), node[7])
             if node[3] == "VERB":
                 verb_locs.append(int(node[0]))
@@ -24,9 +27,11 @@ def Trees(sentences):
 
         #Second pass to create trees
         verb_child = {}
-        placed_Verb = 0
-        need_Verb = 0
         for node in sentence:
+
+            #Skipping over enchanced nodes
+            if ".1" in node[0]:
+                continue
             index = int(node[0])
             head = int(node[6])
             pos = node[3]
@@ -41,22 +46,10 @@ def Trees(sentences):
                     trees[tree][1] += 1
 
                 if pos != "PUNCT":
-                    print node
-                    if head < index and not placed_Verb:
-                        need_Verb = 1
                     if head not in verb_child:
-                        if need_Verb:
-                            verb_child[head] = [['VERB'],[pos, dep]]
-                            placed_Verb = 1
-                            need_Verb = 0
-                        else:
-                            verb_child[head] = [[pos, dep]]
+                        verb_child[head] = [[index, pos, dep]]
                     else:
-                        if need_Verb:
-                            verb_child[head].append(['VERB'])
-                            placed_Verb = 1
-                            need_Verb = 0
-                        verb_child[head].append([pos, dep])
+                        verb_child[head].append([index, pos, dep])
 
             elif head in verb_locs and pos == "AUX":
                 black_node = pos
@@ -89,48 +82,48 @@ def Trees(sentences):
                         trees[tree][1] += 1
         
 
-        print verb_child
         for verb_index in verb_child:
             crossed_Verb = 0
             tree = ''
             for child in verb_child[verb_index]:
-                if child[0] != 'VERB':
-                    if not crossed_Verb:
-                        tree += child[0] + '<---'+child[1]+'----' 
-                    else:
-                        tree += '----' + child[1] + '--->' + child[0]
-                else:
-                    tree += "VERB"
+                if child[0] > verb_index and not crossed_Verb:
+                    tree += "VERB(black_node)"
                     crossed_Verb = 1
+                    tree += '----' + child[2] + '--->' + child[1] + "   "
+                elif crossed_Verb:
+                    tree += '----' + child[2] + '--->' + child[1] + "   "
+                else:
+                    tree += "   " + child[1] + '<---' + child[2] + '----   '
             if tree not in trees:
                 trees[tree] = ["d"+str(num), 1]
                 num += 1
             else:
                 trees[tree][1] += 1
 
-        '''
-        output = []
-        for tree in trees:
-            if not output:
-                output.append([trees[tree], tree])
-            else:
-                been_inserted = 0
-                for x in range(len(output)):
-                    if trees[tree][0] < output[x][0][0]:
-                        output.insert(x, [trees[tree], tree])
-                        been_inserted = 1
-                        break
-                if not been_inserted:
-                    output.append([trees[tree], tree])
-        '''
-
+    '''
     for tree in trees:
         print trees[tree], tree
+    '''
+    subset = []
+    global_max = 0
+    global_info = []
+    num = 2
+    for tree in trees:
+        if trees[tree][1] > num:
+            subset.append([trees[tree], tree])
+            if trees[tree][1] > global_max:
+                global_max = trees[tree][1]
+                global_info = [trees[tree],tree]
+
+    print "total: ", len(trees)
+    print "trees with > ", num, " : ", len(subset)
+    print "trees with <= ", num," : ", len(trees) - len(subset)
+    print "tree with largest count: ", global_info
 
 
 if __name__ == '__main__':
 
-    #data_file = "./ud-treebanks-conll2017/UD_English/en-ud-train.conllu"
-    data_file = "./reduced.conllu"
+    data_file = "./ud-treebanks-conll2017/UD_English/en-ud-train.conllu"
+    #data_file = "./reduced.conllu"
     texts, sentences = s.extractSentences(data_file)
     Trees(sentences)
