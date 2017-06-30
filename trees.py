15997ba1c935b264dfe83c876351a2eee06e40bd
forrestdavis <fld2111@columbia.edu>
2017-06-30 03:14:35 -0600

    Tree extraction is working! Producing 5247 treesgit add -A!

---
+++ trees.py
@@ -0,0 +1,177 @@
+##############################################################
+##############################################################
+#Code for extracting elementary trees from the universal 
+#dependency data set.
+#
+#Forrest Davis
+#June 30, 2017
+##############################################################
+##############################################################
+
+#Returns dictionary, key: value = dependency: 0/1
+#if 0, dep represents a substitution node
+#if 1, dep represents an adjunction node
+def Deps(dep_file):
+    deps_info = open(dep_file, 'r')
+    deps = {}
+
+    for line in deps_info:
+        line = line.strip()
+        if "#" not in line:
+            line = line.split(' ')
+            
+            if len(line) == 2:
+                deps[line[0]] = line[1]
+    
+    deps_info.close()
+    return deps
+
+#Function for scraping sentence for trees and writing to file
+def extract(num, trees, sentence_hash, arguments, deps, output, out):
+    
+    for word in output:
+
+        dep = word[7]
+        #Skip enhanced nodes
+        if dep == "_":
+            continue
+        #Currently marking all punctuation with black node
+        if dep == "punct":
+            tree = word[3]+'\t'+'b\n'
+            if tree in trees:
+                tree_num = trees[tree][0]
+                trees[tree][1] += 1
+            else:
+                tree_num = 'd'+str(num[0])
+                trees[tree] = [tree_num, 1]
+                num[0]+=1
+            word.append(tree_num)
+            out.write('\t'.join(word)+'\n')
+        #Word is argument of verb, thus is only black node
+        elif(word[6] in arguments and deps[dep]=='0' 
+                and word[3]!="VERB"):
+            tree = word[3]+'\t'+ 'b\n'
+            if tree in trees:
+                tree_num = trees[tree][0]
+                trees[tree][1] += 1
+            else:
+                tree_num = 'd'+str(num[0])
+                trees[tree] = [tree_num, 1]
+                num[0]+=1
+            word.append(tree_num)
+            out.write('\t'.join(word)+'\n')
+        #Word is a verb 
+        elif word[0] in arguments:
+            crossed_Verb = 0
+            tree = ''
+            for arg in arguments[word[0]]:
+                if int(arg) > int(word[0]) and not crossed_Verb:
+                    tree+= '\t'+word[3]+'\t'+'b\n'
+                    tree+= '  '+sentence_hash[arg][2]+'\n'
+                    tree+= sentence_hash[arg][0]+'\tw\n'
+                    crossed_Verb = 1
+                elif crossed_Verb:
+                    tree+= '  '+sentence_hash[arg][2]+'\n'
+                    tree+= sentence_hash[arg][0]+'\tw\n'
+                else:
+                    tree+= sentence_hash[arg][0]+'\tw\n'
+                    tree+= '  '+sentence_hash[arg][2]+'\n'
+            if tree in trees:
+                tree_num = trees[tree][0]
+                trees[tree][1] += 1
+            else:
+                tree_num = 'd'+str(num[0])
+                trees[tree] = [tree_num, 1]
+                num[0]+=1
+            word.append(tree_num)
+            out.write('\t'.join(word)+'\n')
+
+        #Word is a adjunct
+        elif deps[dep] == '1':
+            head = sentence_hash[word[0]][1]
+            index = word[0]
+            if int(head) < int(index):
+                tree = sentence_hash[head][0]+'\t'+'w\n'
+                tree += '  '+dep+'\n'
+                tree += '\t'+word[3]+'\t'+'b\n'
+                if tree in trees:
+                    tree_num = trees[tree][0]
+                    trees[tree][1] += 1
+                else:
+                    tree_num = 'd'+str(num[0])
+                    trees[tree] = [tree_num, 1]
+                    num[0]+=1
+                word.append(tree_num)
+                out.write('\t'.join(word)+'\n')
+            else:
+                tree = '\t'+sentence_hash[head][0]+'\t'+'w\n'
+                tree += '  '+dep+'\n'
+                tree += word[3]+'\t'+'b\n'
+                if tree in trees:
+                    tree_num = trees[tree][0]
+                    trees[tree][1] += 1
+
+                else:
+                    tree_num = 'd'+str(num[0])
+                    trees[tree] = [tree_num, 1]
+                    num[0]+=1
+                word.append(tree_num)
+                out.write('\t'.join(word)+'\n')
+            
+    out.write('\n')
+    
+
+def Trees(data_file, dep_file, output_file):
+    #List to be mutable
+    num = [1]
+    deps = Deps(dep_file)
+    data = open(data_file, 'r')
+    out = open(output_file, 'w')
+
+    #Dictionary for trees, key: value = tree: [tree number, count]
+    trees = {}
+    #key: value == index: POS, HEAD INDEX, DEP LABEL
+    sentence_hash = {}
+    #For writing to file
+    output = []
+    #Dictionary marking arguments, key: value = head: arg1, arg2, etc
+    arguments = {}
+
+    for line in data:
+        line = line.strip()
+        if not line:
+
+            #Extract trees
+            extract(num, trees, sentence_hash, arguments, 
+                    deps, output, out)
+
+            #Reset variables for next sentence
+            sentence_hash = {}
+            output = [] 
+            arguments = {}
+
+        #Create hash map with words in sentence and 
+        #add arguments to seperate hash map
+        elif line and line[0] != "#":
+            line = line.split()
+            output.append(line)
+            if line[6] != "_":
+                sentence_hash[line[0]] = (line[3], line[6], line[7])
+
+                if line[7]!='root' and line[7]!='punct' and deps[line[7]]=='0':
+                    if line[6] not in arguments:
+                        arguments[line[6]] = [line[0]]
+                    else:
+                        arguments[line[6]].append(line[0])
+
+    data.close()
+    out.close()
+    print len(trees)
+
+if __name__ == '__main__':
+
+    data_file = "./ud-treebanks-conll2017/UD_English/en-ud-train.conllu"
+    dep_file = "./possible_deps.txt"
+    output_file = './en-ud-train.conll16'
+
+    Trees(data_file, dep_file, output_file)
